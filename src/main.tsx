6597adcb7f0408822c96085740b4a85817db433b
jigonzalez930209 <jigonzalez930209@gmail.com>
2026-04-12 22:26:26 -0300

    feat: implement routing with HashRouter and add FullLayout component

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import 'katex/dist/katex.min.css'
 import '@/features/i18n/i18n'
 import './index.css'
 import MinimalistLayout from './layouts/minimalist/index.tsx'
+import { FullLayout } from './layouts/full/index.tsx'
 import { AppProviders } from '@/app/providers'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppProviders>
-      <MinimalistLayout />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<MinimalistLayout />} />
+          <Route path="/full" element={<FullLayout />} />
+        </Routes>
+      </HashRouter>
     </AppProviders>
   </StrictMode>,
 )
