1c15a08f1f58ea5f98ce9d785e328861f8b27555
Jose <jigonzalez930209@gmail.com>
2026-04-13 15:41:25 -0300

    feat: add ProLayout route and setup testing environment

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import '@/features/i18n/i18n'
 import './index.css'
 import MinimalistLayout from './layouts/minimalist/index.tsx'
 import { FullLayout } from './layouts/full/index.tsx'
+import { ProLayout } from './layouts/pro/index.tsx'
 import { AppProviders } from '@/app/providers'
 
 createRoot(document.getElementById('root')!).render(
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
         <Routes>
           <Route path="/" element={<MinimalistLayout />} />
           <Route path="/full" element={<FullLayout />} />
+          <Route path="/pro" element={<ProLayout />} />
         </Routes>
       </HashRouter>
     </AppProviders>
